316224b0183449e4d30c18f80ac6f46704299c33
D.Hoeggerl <hoeggerl.daniel@gmail.com>
2013-07-23 11:59:07 +0200

    removed extra sensibility for samsung devices

--- library/src/com/slidingmenu/lib/CustomViewBehind.java
+++ library/src/com/slidingmenu/lib/CustomViewBehind.java
@@ -326,9 +326,9 @@ public class CustomViewBehind extends ViewGroup {
 
 	public boolean menuTouchInQuickReturn(View content, int currPage, float x) {
 		if (mMode == SlidingMenu.LEFT || (mMode == SlidingMenu.LEFT_RIGHT && currPage == 0)) {
-			return x >= content.getLeft() - mMarginThreshold;
+			return x >= content.getLeft();
 		} else if (mMode == SlidingMenu.RIGHT || (mMode == SlidingMenu.LEFT_RIGHT && currPage == 2)) {
-			return x <= content.getRight() + mMarginThreshold;
+			return x <= content.getRight();
 		}
 		return false;
 	}
